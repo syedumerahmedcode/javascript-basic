3b72f0020bd0f4572fdc19669491ed025c8ee395
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-20 11:11:35 +0100

    Feature: This example shows how one can print multiple variable within the a single console log output."

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,15 +1,9 @@
 //document.write(2 + 2);
 // alert("Hello people!");
 let personName = "John Smith";
+let address, postalCode, city;
+address = "Main street 100";
+postalCode = "12345";
+city = "Stockholm";
 
-console.log("My name is " + personName);
-// some code
-
-console.log("My name is " + personName);
-// some code
-console.log("My name is " + personName);
-// some code
-console.log("My name is " + personName);
-// some code
-console.log("My name is " + personName);
-// some code
+console.log("The Bio data is " + personName, address, postalCode, city);
