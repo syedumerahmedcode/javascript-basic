d95efd64c9689417a9e737885a3ea9347cff63a2
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-22 21:04:14 +0100

    Feature: Examples of implicit coversion.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,8 +1,14 @@
-// Number
-// loosely typed = don't declare type
-const integerNumber = 34;
-let decimalNumber = 3.4;
-// decimalNumber = "are great";
+const firstName = "John";
+const lastName = "Smith";
 
-console.log(integerNumber);
-console.log(decimalNumber);
+let value = firstName - lastName;
+console.log("The value is: " + value);
+
+let number = "10";
+let number2 = "12";
+// Implicit type conversion to numbers
+let result = number2 - number;
+console.log("The result is: " + result);
+// Now, Java script is doing string concatination
+result = number2 + number;
+console.log("The result is: " + result);
