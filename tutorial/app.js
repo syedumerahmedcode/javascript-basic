ab18aff6aad7bcf59c23cc4bc507168606f6b971
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-23 21:18:22 +0100

    Feature: Inplemented functions declarations and invocation.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,27 +1,21 @@
 // Arrays, Functions and objects
-// Arrays - [], zero based
-const friend1 = "Anna";
-const friend2 = "Anna";
-const friend3 = "Anna";
-const friend4 = "Anna";
+// Functions - declare, invoke
 
-// how to set up an array--->
-const friends = ["john", "Susan", "peter", "Bob", 45, undefined, null];
-
-let bestFriend = friends[3];
-
-console.log(friends);
-
-console.log("The first friend is: " + friends[0]);
-
-console.log("The third value in the friend array is: " + friends[2]);
-
-console.log("If I access index 13, I will get: " + friends[13]);
-
-console.log("My best friend is: " + bestFriend);
-
-friends[4] = "Anna";
-
-console.log(friends);
-
-console.log("My new firend is: " + friends[4]);
+/**
+ * This is function declaration.
+ */
+function hello() {
+  // logic
+  console.log("Hello there Bob");
+  console.log("Hello there Susy");
+  console.log("Hello there Alice");
+}
+/**
+ * This is function invocation
+ */
+hello();
+// some more code...
+hello();
+// some more code...
+hello();
+// some more code...
