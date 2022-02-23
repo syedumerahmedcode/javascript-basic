1e0e46013ac9a631c84e3787a27f2891a0008047
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-23 20:52:39 +0100

    Feature: Added examples of Array, how to create and use it and how to manipulate it.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,14 +1,27 @@
-const firstName = "John";
-const lastName = "Smith";
+// Arrays, Functions and objects
+// Arrays - [], zero based
+const friend1 = "Anna";
+const friend2 = "Anna";
+const friend3 = "Anna";
+const friend4 = "Anna";
 
-let value = firstName - lastName;
-console.log("The value is: " + value);
+// how to set up an array--->
+const friends = ["john", "Susan", "peter", "Bob", 45, undefined, null];
 
-let number = "10";
-let number2 = "12";
-// Implicit type conversion to numbers
-let result = number2 - number;
-console.log("The result is: " + result);
-// Now, Java script is doing string concatination
-result = number2 + number;
-console.log("The result is: " + result);
+let bestFriend = friends[3];
+
+console.log(friends);
+
+console.log("The first friend is: " + friends[0]);
+
+console.log("The third value in the friend array is: " + friends[2]);
+
+console.log("If I access index 13, I will get: " + friends[13]);
+
+console.log("My best friend is: " + bestFriend);
+
+friends[4] = "Anna";
+
+console.log(friends);
+
+console.log("My new firend is: " + friends[4]);
