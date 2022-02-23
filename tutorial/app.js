a598cf1de62c819eff2372a2d96b9d31719c836b
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-23 21:57:19 +0100

    Feature: Now using parameters to invoke same function with different arguments.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,21 +1,23 @@
 // Arrays, Functions and objects
 // Functions - declare, invoke
+// params - when declare/define placeholders, local vars
+// arguments -when invole/call/run use var/values, multiple params, undefined
 
 /**
  * This is function declaration.
  */
-function hello() {
+function hello(personName) {
   // logic
-  console.log("Hello there Bob");
-  console.log("Hello there Susy");
-  console.log("Hello there Alice");
+  console.log("Hello there, " + personName);
 }
 /**
  * This is function invocation
  */
+// greet Bob
+hello("Bob");
+// greet Anna
+hello("Anna");
+// greet Susy
+hello("Susy");
+// The following thing prints undefined in console log if no argument is passed.
 hello();
-// some more code...
-hello();
-// some more code...
-hello();
-// some more code...
