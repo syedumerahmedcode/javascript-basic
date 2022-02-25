e31840f43f42b93000e760347ae53fd765d7dca8
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-25 23:40:33 +0100

    Feature: Example of function expression and how to use it.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,22 +1,18 @@
-// Arrays, Functions and objects
-// Functions - declare, invoke
-// return
-// default undefined, shortcuts, ignores after...
-// 1 inch = 2.54 cm
-const wallHeight = 80;
-const wallWidth = 100;
+// Assigning a function to a variable instead of its value.
 
-/**
- * NOTE: Please note that if a method does not have a return type specified, then all functions by default in javascript return undefined.
- * @param {Some value, in our case it is a int specifying inches.} value
- */
-function calculate(value) {
-  // If string is returned, the the console log for dimensions will contain the string specified in the return statement
-  // return "Hello world";
-  // Now the function is returned as a decimal number.
-  return value * 2.54;
+function calculate(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 }
-const width = calculate(wallWidth);
-const height = calculate(wallHeight);
-const dimensions = [width, height];
-console.log(dimensions);
+const firstValue = calculate(3, 4);
+const secondValue = calculate(10, 36);
+
+// function expression
+const add = function (firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+};
+// How to use a function expression
+const thirdValue = add(3, 4);
+const fourthValue = add(10, 36);
+const values = [firstValue, secondValue, thirdValue, fourthValue];
+
+console.log(values);
