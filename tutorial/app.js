c91b7dffcfc244ce445639d1e17821523b45b44c
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-26 10:59:48 +0100

    Feature: Basic conditional statement implemented.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,26 +1,10 @@
-// Objects - key/value pairs methods
-// dot notation
+// Conditional statements
+// >, <, >=, <=, ==, ===, !=, !===
 
-const person = {
-  firstName: "John",
-  lastName: "Smith",
-  age: 40,
-  education: false,
-  married: true,
-  siblings: ["Alice", "Bob", "Carol"],
-  greeting: function () {
-    console.log("Hello, my name is John");
-  },
-};
-
-console.log(
-  "The name of the person is " + person.firstName + " " + person.lastName
-);
-const age = person.age;
-console.log("The age of the person is " + age);
-person.education = true;
-const isPersonEducated = person.education;
-console.log("Is the person educated: " + isPersonEducated);
-console.log("Is person married: " + person.married);
-console.log("The person has the following siblings: " + person.siblings);
-person.greeting();
+const value = 2 > 1;
+console.log(typeof value);
+if (value) {
+  console.log("Hello world");
+} else {
+  console.log("Hello people.");
+}
