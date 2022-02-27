11b48d708435cd3606b116961c2eb9c6ac6d1313
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-27 13:46:08 +0100

    Feature: example of using logical operators in javascript - work in progress.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,11 +1,10 @@
-// Conditional statements
-// >, <, >=, <=, ==, ===, !=, !===
-// else if and !
+// Logical operators
+// (|| - OR), (&& - AND), !==> NOT
 
-const num1 = 6;
-const num2 = "6";
-const value = num1 == num2;
-
-const value2 = num1 === num2;
-console.log("== comparison corresponds to " + value);
-console.log("=== comparison corresponds to " + value2);
+const name = "Bob";
+const age = 24;
+if (name === "Bob" && age == 24) {
+  console.log("Hello there user.");
+} else {
+  console.log("Wrong values");
+}
