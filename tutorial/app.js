744c54badae738fcedcdcf1426cadc8487f25ed7
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-27 14:13:17 +0100

    Feature: Implemented switch case scenario example for a dice.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,10 +1,29 @@
-// Logical operators
-// (|| - OR), (&& - AND), !==> NOT
+// switch case statements
+// dice values: 1 - 6
+// without break, all switch case below the matching criteria will be executed because of the fall through mechanism
 
-const name = "Bob";
-const age = 24;
-if (name === "Bob" && age == 24) {
-  console.log("Hello there user.");
-} else {
-  console.log("Wrong values");
+const dice = 1;
+
+switch (dice) {
+  case 1:
+    console.log("You got one");
+    break;
+  case 2:
+    console.log("You got two");
+    break;
+  case 3:
+    console.log("You got three");
+    break;
+  case 4:
+    console.log("You got four");
+    break;
+  case 5:
+    console.log("You got five");
+    break;
+  case 6:
+    console.log("You got six");
+    break;
+  default:
+    console.log("You did not roll the dice.");
+    break;
 }
