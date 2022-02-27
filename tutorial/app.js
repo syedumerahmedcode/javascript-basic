c798e590fbb404bc30b55291ea613cb5a567a6a6
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-27 14:35:16 +0100

    Feature: Added an example of while loop

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,29 +1,12 @@
-// switch case statements
-// dice values: 1 - 6
-// without break, all switch case below the matching criteria will be executed because of the fall through mechanism
+// Loops
+// repeatedly run a block of code while the condition is true
+// while loop
 
-const dice = 1;
-
-switch (dice) {
-  case 1:
-    console.log("You got one");
-    break;
-  case 2:
-    console.log("You got two");
-    break;
-  case 3:
-    console.log("You got three");
-    break;
-  case 4:
-    console.log("You got four");
-    break;
-  case 5:
-    console.log("You got five");
-    break;
-  case 6:
-    console.log("You got six");
-    break;
-  default:
-    console.log("You did not roll the dice.");
-    break;
+let amount = 10;
+while (amount > 0) {
+  console.log("I have " + amount + " dollars and I'm going to the mall");
+  amount--;
+  if (amount == 0) {
+    console.log("I am broke as I have " + amount + " dollars");
+  }
 }
