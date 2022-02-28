ad08437b12d0047a8db4eeec4405b12ad0a0ee3e
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-28 22:55:15 +0100

    Feature: Using template lietrals and interpolation in the example.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,24 +1,6 @@
-// String properties and methods
-// wrapper String object, don't memorize methods
-
-let text = "John Smith";
-let result = text.length;
-console.log("The number of characters in the name are: " + result);
-console.log(text.toLocaleUpperCase());
-console.log(text.charAt(0));
-console.log(text.indexOf("ß"));
-console.log(text.trim().length);
-console.log(text.slice(0, 2));
-console.log(text.slice(-4));
-
-const person = {
-  name: "John", // property
-  greeting() {
-    // method
-    console.log("Hey, I am John");
-  },
-};
-
-console.log(person);
-console.log(person.name);
-person.greeting();
+// Template literals
+// interpolation ${}
+const name = "John";
+const age = 25;
+const sentence = `Hey it's ${name} and he is ${age} years old.`;
+console.log(sentence);
