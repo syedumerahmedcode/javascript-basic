922295f3a71febadc45637d0c31373f4587a9dda
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-28 22:37:39 +0100

    Feature: Using various string functions including slice - forward as well as backwards.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,11 +1,15 @@
 // String properties and methods
 // wrapper String object, don't memorize methods
 
-let text = "John Smith        ";
+let text = "John Smith";
 let result = text.length;
 console.log("The number of characters in the name are: " + result);
 console.log(text.toLocaleUpperCase());
 console.log(text.charAt(0));
+console.log(text.indexOf("ß"));
+console.log(text.trim().length);
+console.log(text.slice(0, 2));
+console.log(text.slice(-4));
 
 const person = {
   name: "John", // property
