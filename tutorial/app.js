9a9cbf1ea12a8c92587b1d5d18d8386d822455c8
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-02-28 21:56:40 +0100

    Feature: Using string functions in javascript - work in progress.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,9 +1,11 @@
 // String properties and methods
 // wrapper String object, don't memorize methods
 
-let text = "John Smith";
+let text = "John Smith        ";
 let result = text.length;
 console.log("The number of characters in the name are: " + result);
+console.log(text.toLocaleUpperCase());
+console.log(text.charAt(0));
 
 const person = {
   name: "John", // property
