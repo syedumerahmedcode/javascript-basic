5d5abd4d8b400715f799c38c3f82e67f18b1a479
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-02 21:40:02 +0100

    Feature: Using some basic methods of an array in javascript.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,6 +1,33 @@
-// Template literals
-// interpolation ${}
-const name = "John";
-const age = 25;
-const sentence = `Hey it's ${name} and he is ${age} years old.`;
-console.log(sentence);
+// Arrays propertoes and methods
+let names = ["Alice", "Bob", "Olga", "John"];
+//length
+console.log(names.length);
+
+//concat
+const lastNames = ["Pepper", "onion", "Banana"];
+const allNames = names.concat(lastNames);
+console.log(allNames);
+
+// reverse
+console.log(allNames.reverse());
+
+//unshift - adds to the beginning of the array
+allNames.unshift("Susy");
+console.log(allNames);
+
+//shift - removes to the beginning of the array
+allNames.shift();
+console.log(allNames);
+
+//push - adds to the end of the array
+allNames.push("Susy");
+console.log(allNames);
+
+//pop - removes to the end of the array
+allNames.pop();
+console.log(allNames);
+
+//splice - mutates original array
+const specificNames = allNames.splice(2, 1);
+console.log(allNames);
+console.log(specificNames);
