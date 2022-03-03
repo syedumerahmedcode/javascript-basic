fddb07cf39fde4de8f96851a702b2e9fd137d249
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-03 22:54:08 +0100

    Feature: Wrote teh description of value and referénce in a comment.

---
+++ tutorial/app.js
@@ -0,0 +1,15 @@
+/**
+ * Reference vs value
+ * Primitive data types
+ * String, Number, Symbol, Boolean, Undefined, Null
+ * Arrays, Functions, Objects = object
+ * typeof
+ *
+ * when assigning primitive data type to a variable,
+ * any changes are made directly to that value,
+ * without affecting original value.
+ *
+ * when assigning non-primitive data type to a variable,
+ * is done by reference so any changes will affect all the references.
+ *
+ */
