da9d9f0f22ec22bfe0d6825ecb1a8281e804ea67
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-03 22:05:33 +0100

    Feature: Using arrays together with for loop.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,33 +1,15 @@
-// Arrays propertoes and methods
-let names = ["Alice", "Bob", "Olga", "John"];
-//length
-console.log(names.length);
+// Arrays and for loops
+const names = ["Adam", "Susy", "Bob"];
+const lastName = "ShakeAndBake";
+let newArray = [];
 
-//concat
-const lastNames = ["Pepper", "onion", "Banana"];
-const allNames = names.concat(lastNames);
-console.log(allNames);
+// for loop
+for (let i = 0; i < names.length; i++) {
+  console.log(i);
+  console.log(names[i]);
+  const fullName = `${names[i]} ${lastName}`;
+  newArray.push(fullName);
+}
 
-// reverse
-console.log(allNames.reverse());
-
-//unshift - adds to the beginning of the array
-allNames.unshift("Susy");
-console.log(allNames);
-
-//shift - removes to the beginning of the array
-allNames.shift();
-console.log(allNames);
-
-//push - adds to the end of the array
-allNames.push("Susy");
-console.log(allNames);
-
-//pop - removes to the end of the array
-allNames.pop();
-console.log(allNames);
-
-//splice - mutates original array
-const specificNames = allNames.splice(2, 1);
-console.log(allNames);
-console.log(specificNames);
+console.log(names);
+console.log(newArray);
