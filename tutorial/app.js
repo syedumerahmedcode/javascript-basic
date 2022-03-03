ac9de59fedf6c7a4fb9a628caf5c27100ba6afbd
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-03 22:33:47 +0100

    Feature: Doing console log of multiple values in a unique way.

--- tutorial/app.js
+++ tutorial/app.js
@@ -5,8 +5,22 @@ const food = [10, 40, 50];
 function calculate(arr) {
   let total = 0;
   for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+    // console.log(arr[i]);
+    total += arr[i];
   }
+  // console.log("The total is " + total);
+  return total;
 }
 
-calculate(food);
+const gasTotal = calculate(gas);
+const foodTotal = calculate(food);
+const randomTotal = calculate([200, 4000, 500, 1]);
+console.log("The total for gas is " + gasTotal);
+console.log("The total for food is " + foodTotal);
+console.log("The total for randam array is " + randomTotal);
+// Or a more interesting way of doing console log together
+console.log({
+  gas: gasTotal,
+  food: foodTotal,
+  random: randomTotal,
+});
