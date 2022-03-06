9fdf18be6594a36bec251d0fa6e950c4244e2d8f
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-06 08:49:31 +0100

    Feature: Implemented examples showing how variable lookup works.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,26 +1,23 @@
-/**
- * Local scope
- * cannot access from outside code block
- * if - NOT VAR
- */
-let name = "bobo";
+// variable lookup
+// {} - code look up
 
-function calculate() {
-  const name = "John";
-  const age = 25;
-  test = "global visible text";
-}
-calculate();
-// age is not found as it test is a  is a s a local variable and
-//hence it is out of scope here which results in an exception
-console.log(age);
-// Even though 'test' is defined within the function(),
-// it does not have let or const in front of it
-// Javascript interprets this as a global variable
-// Strange,I know! but this is how JS works
-console.log(test);
+const globalNumber = 5;
 
-if (true) {
-  const name = "John";
+function add(num1, num2) {
+  /**
+   * The way JS works is that it looks up a variable in the
+   * local scope first, then in the global scope. That means
+   * if const globalNumber = 20;is commented in, it is
+   * given precedence by JS
+   */
+  //const globalNumber = 20;
+  const result = num1 + num2 + globalNumber;
+  function multiply() {
+    const multiplyResult = result * globalNumber;
+    console.log(multiplyResult);
+  }
+  multiply();
+  return result;
 }
-console.log(`My name is ${name}`);
+
+console.log(add(3, 4));
