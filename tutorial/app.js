04bfe7c5c0f9dd68129450d4ef730475d071b180
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-07 20:39:00 +0100

    Feature: Implemented examples of map using callback function.

--- tutorial/app.js
+++ tutorial/app.js
@@ -6,33 +6,28 @@
  * call callback against each item in the array.
  * Reference Item in the callback parameter.
  */
-/**
- * forEach
- * does not return new array
- */
 
+/**
+ * map - does return a new array .
+ * does not change size of original array.
+ * uses values from original array when
+ * making a new one.
+ */
 const people = [
   { name: "Bob", age: 20, position: "developer" },
   { name: "Peter", age: 25, position: "designer" },
   { name: "Susy", age: 30, position: "the boss" },
+  { name: "Anna", age: 35, position: "the big boss" },
 ];
-/**
- * Callback function which is called from forEach() function.
- * @param {Person Object} person
- */
-function showPerson(person) {
-  console.log(person.position.toUpperCase());
-}
 
-/**
- * ForEach is a higher order function that accepts a callback
- * function --> which we defined in your javascript file.
- */
-people.forEach(showPerson);
-
-/**
- * forEach() used via anonymous function.
- */
-people.forEach(function (item) {
-  console.log(item.age + 10);
+const ages = people.map(function (person) {
+  return person.age + 20;
 });
+console.log(ages);
+const newPeople = people.map(function (person) {
+  return {
+    firstName: person.name.toUpperCase(),
+    oldAge: person.age + 20,
+  };
+});
+console.log(newPeople);
