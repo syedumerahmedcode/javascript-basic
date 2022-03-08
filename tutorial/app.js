7d5955a47a92f9b28f7b532587655245360ece96
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-08 21:42:49 +0100

    Feature: Created an example for array.find

--- tutorial/app.js
+++ tutorial/app.js
@@ -9,13 +9,28 @@
 
 /**
  * find
- * returns object
+ * returns a single instance - (in this case object)
  * returns first match, if no match undefined
  * great for getting unique value
  */
 const people = [
-  { name: "Bob", age: 20, position: "developer" },
-  { name: "Peter", age: 25, position: "designer" },
-  { name: "Susy", age: 30, position: "the boss" },
-  { name: "Anna", age: 35, position: "the big boss" },
+  { name: "Bob", age: 20, position: "developer", id: 1 },
+  { name: "Peter", age: 25, position: "designer", id: 2 },
+  { name: "Susy", age: 30, position: "the boss", id: 3 },
+  { name: "Anna", age: 35, position: "the big boss", id: 4 },
 ];
+
+const boss = people.find(function (person) {
+  return person.position.includes("boss");
+});
+console.log(boss);
+
+const personById4 = people.find(function (person) {
+  return person.id === 4;
+});
+console.log(personById4);
+
+const personUnknown = people.find(function (person) {
+  return person.id === 344;
+});
+console.log(personUnknown);
