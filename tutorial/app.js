5518bbaa2058b4fa57b2113f188fc44881fb0b50
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-11 20:25:17 +0100

    Feature: Implemented an example of array.reduce() function.

--- tutorial/app.js
+++ tutorial/app.js
@@ -15,21 +15,15 @@
  * 2 parameter ('curr') - current iteration/value
  */
 const people = [
-  { name: "Bob", age: 20, position: "developer", id: 1 },
-  { name: "Peter", age: 25, position: "designer", id: 2 },
-  { name: "Susy", age: 30, position: "the boss", id: 3 },
-  { name: "Anna", age: 35, position: "the big boss", id: 4 },
+  { name: "Bob", age: 20, position: "developer", id: 1, salary: 1000 },
+  { name: "Peter", age: 25, position: "designer", id: 2, salary: 3000 },
+  { name: "Susy", age: 30, position: "the boss", id: 3, salary: 8000 },
+  { name: "Anna", age: 35, position: "the big boss", id: 4, salary: 8000 },
 ];
 
-//const boss = people.reduce
-console.log(boss);
-
-const personById4 = people.find(function (person) {
-  return person.id === 4;
-});
-console.log(personById4);
-
-const personUnknown = people.find(function (person) {
-  return person.id === 344;
-});
-console.log(personUnknown);
+const total = people.reduce(function (acc, currItem) {
+  console.log(`current salary: ${currItem.salary}`);
+  acc += currItem.salary;
+  console.log(`acc: ${acc}`);
+  return acc;
+}, 0);
