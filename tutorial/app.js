a59fd75704f8063ee36372e2be9021a7504c531e
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-12 10:45:36 +0100

    Feature: Implemented the challenge that uses map function on array.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,2 +1,7 @@
-console.log(students);
-sayName("John");
+// We already have access to students from data.js
+const updatedStudent = students.map(function (currentStudent) {
+  // console.log(currentStudent);
+  currentStudent.role = "student";
+  return currentStudent;
+});
+console.log(updatedStudent);
