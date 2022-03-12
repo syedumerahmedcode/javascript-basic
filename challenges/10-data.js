4b0692a91fd3771acd9ab320d6bf847f1d183026
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-12 09:21:48 +0100

    Feature: Created a data file, the reference from which is used for solving the array challenge in app.js

---
+++ challenges/10-data.js
@@ -0,0 +1,7 @@
+const students = [
+  { id: 1, name: "peter", score: 80, favouriteSubject: "maths" },
+  { id: 2, name: "Alice", score: 99, favouriteSubject: "english" },
+  { id: 3, name: "Bob", score: 77, favouriteSubject: "maths" },
+  { id: 4, name: "Susy", score: 60, favouriteSubject: "history" },
+  { id: 5, name: "Carrey", score: 82, favouriteSubject: "maths" },
+];
--- tutorial/app.js
+++ tutorial/app.js
@@ -1,9 +1,1 @@
-const students = [
-  { id: 1, name: "peter", score: 80, favouriteSubject: "maths" },
-  { id: 2, name: "Alice", score: 99, favouriteSubject: "english" },
-  { id: 3, name: "Bob", score: 77, favouriteSubject: "maths" },
-  { id: 4, name: "Susy", score: 60, favouriteSubject: "history" },
-  { id: 5, name: "Carrey", score: 82, favouriteSubject: "maths" },
-];
-
 console.log(students);
