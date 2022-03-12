8e39987ae16fbf229155c699c508a34149a6b9e0
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-12 13:14:44 +0100

    Feature: Implemented an example of getting an average of all students using map.reduce() function.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,25 +1,9 @@
 // We already have access to students from data.js
-const updatedStudent = students.map(function (currentStudent) {
-  // console.log(currentStudent);
-  currentStudent.role = "student";
-  return currentStudent;
-});
-// console.log(updatedStudent);
+const averageScore =
+  students.reduce(function (scoresTotal, currentStudent) {
+    console.log(currentStudent);
+    scoresTotal = scoresTotal + currentStudent.score;
 
-const studentsWithHighScore = students.filter(function (currentStudent) {
-  // if (currentStudent.score >= 80) {
-  //   return currentStudent;
-  // }
-
-  // Following example return the value in a one liner
-  // if (currentStudent.score >= 80) return currentStudent;
-
-  // This works because in the background truthy is working.
-  return currentStudent.score >= 80;
-});
-// console.log(studentsWithHighScore);
-
-const studentWithSpecificId = students.find(function (currentStudent) {
-  return currentStudent.id === 1;
-});
-console.log(studentWithSpecificId);
+    return scoresTotal;
+  }, 0) / students.length;
+console.log(averageScore);
