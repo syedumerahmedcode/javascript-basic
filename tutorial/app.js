454e12e86c7b82274043bd6c5978520535e262a7
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-20 08:23:23 +0100

    Feature: Implementing an example demonstrating the usage of nodeValue and textContent - work in progress.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,25 +1,2 @@
-/**
- * Select the element or the group of elements that we want
- * Decide the effect that we want to apply to the collection
- *
- * previousElementSibling
- * nextElementSibiling
- */
-
-/**
- * since we are searching for class=first,
- * hence, we give a '.'(dot) before the
- * class name
- */
-const first = document.querySelector(".first");
-console.log(first);
-first.nextElementSibling.style.color = "red";
-
-/**
- * since we are searching for id=last,
- * hence, we give a '#'(hashtag) before the
- * id
- */
-const last = document.querySelector("#last");
-console.log(last);
-last.previousElementSibling.style.color = "blue";
+//text content
+// nodeValue
