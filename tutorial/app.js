60f637ea0d710f2408264c3d43059bc1e1b1663f
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-21 07:50:19 +0100

    Feature: Folder restructuring plus working on the next example.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,2 +1,9 @@
 //text content
 // nodeValue
+
+const item = document.getElementById("special");
+const value = item.nodeValue;
+// This retuns null, I am not sure why?
+console.log(value);
+// This returns the correct value
+console.log(item.firstChild.nodeValue);
