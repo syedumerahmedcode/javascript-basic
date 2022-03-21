5da922637ac5cb3bcf0c68f818ca226cf339f9ba
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-21 07:56:40 +0100

    Feature: Created a backup of textContent and nodeValue example.

---
+++ tutorial/DOM basic/textContentAndNodeValue/textContentAndNodeValue.js
@@ -0,0 +1,11 @@
+//text content
+// nodeValue
+
+const item = document.getElementById("special");
+const value = item.nodeValue;
+// This retuns null, I am not sure why?
+console.log(value);
+// This returns the correct value
+console.log(item.firstChild.nodeValue);
+
+console.log(item.textContent);
