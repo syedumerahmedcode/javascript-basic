ca5ecc9c0b79ef3a3f552e5ad1f3915cbdbf2d17
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-21 20:37:24 +0100

    Feature: Implemented an example of getAttribute() and setAttribute() methods.

--- tutorial/app.js
+++ tutorial/app.js
@@ -2,3 +2,22 @@
  * getAttribute()
  * setAttribute()
  */
+
+const first = document.querySelector(".first");
+const classValue = first.getAttribute("class");
+console.log(classValue);
+// const idValue = first.getAttribute("id");
+// console.log(idValue);
+
+const link = document.getElementById("link");
+const showLink = link.getAttribute("href");
+console.log(showLink);
+
+const last = link.nextElementSibling;
+last.setAttribute("class", "first");
+last.textContent = "I also have a class of first";
+console.log(last);
+
+// get a list of all element which has class=first
+const links = document.querySelectorAll(".first");
+console.log(links);
