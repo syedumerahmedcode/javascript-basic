3f50375a8a0824c3b65e827be67e64f93e3db40a
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-26 03:48:51 +0100

    Feature: Implemented example of className.

--- tutorial/app.js
+++ tutorial/app.js
@@ -2,3 +2,19 @@
  * classname
  * classlist
  */
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
+
+const classValue = first.className;
+console.log(classValue);
+
+second.className = "colors";
+third.className = "text";
+/**
+ * NOTE:
+ * className will always override the last class name
+ * with the latest one.In other words, it can only hold
+ * one class name at a time. In order to add more
+ * class names in a DOM object, use classlist
+ */
