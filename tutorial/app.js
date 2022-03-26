a478f5661534c22611083eb4094a60653d0d3036
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-26 06:17:08 +0100

    Feature: Implementing an example of createElement, createTextNode and appendChild - work in progress.

--- tutorial/app.js
+++ tutorial/app.js
@@ -3,3 +3,17 @@
  * createTextNode('text content')
  * element.appendChild(childElement)
  */
+
+const result = document.querySelector("#result");
+// Steps in creating an element and adding it to DOM attribute
+
+// create empty element
+const bodyDiv = document.createElement("div");
+
+//create text node
+const text = document.createTextNode("a simple body div");
+// add text node to body so that it is visible
+bodyDiv.appendChild(text);
+// add bodydiv(containing text node) to the document
+// so that it appears on the screen
+document.body.appendChild(bodyDiv);
