9f81d3d3ef6146961c9a90db2a865e964e84b7dd
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-26 05:02:02 +0100

    Feature: Setup for createElement - createTextNode - appendChild - work in progress.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,35 +1,5 @@
 /**
- * classname
- * classlist
+ * createElement('element')
+ * createTextNode('text content')
+ * element.appendChild(childElement)
  */
-const first = document.getElementById("first");
-const second = document.getElementById("second");
-const third = document.getElementById("third");
-
-const classValue = first.className;
-// console.log(classValue);
-
-first.className = "colors text";
-second.className = "colors";
-// third.className = "text";
-/**
- * NOTE:
- * className will always override the last class name
- * with the latest one.In other words, it can only hold
- * one class name at a time. In order to add more
- * class names in a DOM object, use classlist. Another
- * solution is to give spaces when giving class names.
- */
-
-third.classList.add("text", "colors");
-third.classList.remove("colors");
-let result = third.classList.contains("text");
-if (result) {
-  console.log("It contains the class.");
-} else {
-  console.log("It doesnot contain the class.");
-}
-
-// third.classList.add("colors");
-const classValueForThirdHeading = third.classList;
-console.log(classValueForThirdHeading);
