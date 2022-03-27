801c094c0228c5edd14798ca266fd64bad4f75ca
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-27 06:07:08 +0200

    Feature: Implementing an example of prependInnerText - work in progress.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,26 +1,4 @@
 /**
- * createElement('element')
- * createTextNode('text content')
- * element.appendChild(childElement)
- *
- * insertElement('element', 'location')
+ * prepend
+ * innerText
  */
-
-const result = document.querySelector("#result");
-// Steps in creating an element and adding it to DOM attribute
-
-// create empty element
-const bodyDiv = document.createElement("div");
-
-//create text node
-const text = document.createTextNode("a simple body div");
-bodyDiv.appendChild(text);
-console.log(result.children);
-document.body.insertBefore(bodyDiv, result);
-
-const smallHeading = document.createElement("h6");
-const smallText = document.createTextNode(`I'm a smll heading text`);
-smallHeading.classList.add("red");
-smallHeading.appendChild(smallText);
-
-document.body.replaceChild(smallHeading, bodyDiv);
