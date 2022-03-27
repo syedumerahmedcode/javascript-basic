0c69708b03c3812d920b354587466da1abe43639
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-27 07:07:37 +0200

    Feature: Scffolding for remove and removeChild methods in DOM - work in progress.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,8 +1,4 @@
 /**
- * prepend
- * innerText
+ * remove
+ * removeChild
  */
-
-const heading = document.createElement("h2");
-heading.innerText = `I am a dynamic heading`;
-document.body.prepend(heading);
