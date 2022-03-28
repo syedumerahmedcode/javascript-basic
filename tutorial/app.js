4f700ea0185002e306151309ba8590c9519bd613
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-03-28 22:25:47 +0200

    Feature: Implemented an example of textContent

--- tutorial/app.js
+++ tutorial/app.js
@@ -2,3 +2,9 @@
  * innerHTML
  * textContent
  */
+
+const first = document.getElementById("first");
+const div = document.getElementById("second");
+const item = document.querySelector(".item");
+
+console.log(div.textContent);
