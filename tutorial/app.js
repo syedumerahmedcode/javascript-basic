571acde68cb69f84e509a6d3aeb82c8f88fee467
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-05-30 22:29:57 +0200

    Feature: Created an example of function reference.

--- tutorial/app.js
+++ tutorial/app.js
@@ -8,10 +8,19 @@
 const btn = document.querySelector(".btn");
 const heading = document.querySelector("h2");
 
+// define what to do
+function changeColors() {
+  console.log("hello");
+  // Inthis method, we implement toggling functionality
+  let hasClass = heading.classList.contains("red");
+  if (hasClass) {
+    heading.classList.remove("red");
+  } else {
+    heading.classList.add("red");
+  }
+}
+
 // using addEventListener
 // Here we also describe
-// What event and what to do
-btn.addEventListener("click", function () {
-  console.log("Hey, you clicked the button.");
-  heading.classList.add("red");
-});
+// What event
+btn.addEventListener("click", changeColors);
