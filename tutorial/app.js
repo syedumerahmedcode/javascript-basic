908529e49d109373e3c27ecf7314bebe32aa7e27
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-05-30 22:34:49 +0200

    Feature: Created an empty js file, just with comments, for mouse events lecture part.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,26 +1,7 @@
 /**
- * select element
- * addEventListener()
- * what event, what to do
+ * click - fires after full action occurs
+ * mousedown - button is pressed
+ * mouseup - button is released
+ * mouseenter - moved onto an element
+ * mouseleave - moved out of an element
  */
-
-// Here we are selecting the elements
-const btn = document.querySelector(".btn");
-const heading = document.querySelector("h2");
-
-// define what to do
-function changeColors() {
-  console.log("hello");
-  // Inthis method, we implement toggling functionality
-  let hasClass = heading.classList.contains("red");
-  if (hasClass) {
-    heading.classList.remove("red");
-  } else {
-    heading.classList.add("red");
-  }
-}
-
-// using addEventListener
-// Here we also describe
-// What event
-btn.addEventListener("click", changeColors);
