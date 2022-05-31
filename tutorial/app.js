bcbca5ac0ca61591d1d732d724858eb5bfea4d2b
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-05-31 20:42:38 +0200

    Feature: Created the basic setup for the example demonstrating Key events.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,32 +1,5 @@
 /**
- * click - fires after full action occurs
- * mousedown - button is pressed
- * mouseup - button is released
- * mouseenter - moved onto an element
- * mouseleave - moved out of an element
+ * keypress - when key is pressed
+ * keydown - when key is down
+ * keyup - when key is released
  */
-
-const heading = document.querySelector("h1");
-const btn = document.querySelector(".btn");
-
-btn.addEventListener("click", function () {
-  console.log("You clicked me.");
-});
-
-btn.addEventListener("mousedown", function () {
-  console.log("down.");
-});
-
-btn.addEventListener("mouseup", function () {
-  console.log("up.");
-});
-
-heading.addEventListener("mouseenter", function () {
-  console.log("Mouse entered the heading");
-  heading.classList.add("blue");
-});
-
-heading.addEventListener("mouseleave", function () {
-  console.log("Mouse left the heading");
-  heading.classList.remove("blue");
-});
