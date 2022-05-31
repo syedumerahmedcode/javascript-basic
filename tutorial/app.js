706553a310e9c5ea01c270f930ca71e722213ecb
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-05-31 20:53:18 +0200

    Feature: Created an example for Key events.

--- tutorial/app.js
+++ tutorial/app.js
@@ -3,3 +3,17 @@
  * keydown - when key is down
  * keyup - when key is released
  */
+
+const nameInput = document.getElementById("name");
+
+nameInput.addEventListener("keypress", function () {
+  console.log("keypress");
+});
+
+nameInput.addEventListener("keydown", function () {
+  console.log("keydown");
+});
+
+nameInput.addEventListener("keyup", function () {
+  console.log(nameInput.value);
+});
