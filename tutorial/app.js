efbf98280014bed81a4db11e369c0de87265dc3d
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-05-31 20:36:37 +0200

    Feature: Created an example for mouse events.

--- tutorial/app.js
+++ tutorial/app.js
@@ -5,3 +5,28 @@
  * mouseenter - moved onto an element
  * mouseleave - moved out of an element
  */
+
+const heading = document.querySelector("h1");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+  console.log("You clicked me.");
+});
+
+btn.addEventListener("mousedown", function () {
+  console.log("down.");
+});
+
+btn.addEventListener("mouseup", function () {
+  console.log("up.");
+});
+
+heading.addEventListener("mouseenter", function () {
+  console.log("Mouse entered the heading");
+  heading.classList.add("blue");
+});
+
+heading.addEventListener("mouseleave", function () {
+  console.log("Mouse left the heading");
+  heading.classList.remove("blue");
+});
