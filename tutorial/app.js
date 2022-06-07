13780a2b0b52d8036d8b9849a12b00a1740286cc
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-06-07 19:49:13 +0200

    Feature: Created an example for event propagation, bubbling and capture.

--- tutorial/app.js
+++ tutorial/app.js
@@ -4,3 +4,21 @@
  * event bubbling - clicked element first then bubbles up -- default
  * event capturing - fires at the root and fires until reaches target
  */
+const container = document.querySelector(".container");
+const list = document.querySelector(".list-items");
+
+function showBubbling(e) {
+  console.log("current target", e.currentTarget);
+  //   console.log("Target", e.target);
+  //   if (e.target.classList.contains("link")) {
+  //     console.log("You clicked on the link");
+  //   }
+}
+
+function stopPropagation(e) {
+  console.log("You clicked the link");
+  e.stopPropagation();
+}
+
+list.addEventListener("click", showBubbling, { capture: true });
+container.addEventListener("click", showBubbling, { capture: true });
