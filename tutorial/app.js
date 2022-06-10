c84b42bd058adf405e6150195ce93cb36dcb03b5
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-06-10 19:17:34 +0200

    Feature: Basic setup for forms example.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,23 +1,5 @@
 /**
- * allows select dynamic events
- * event propogation - order the elements are fired
- * event bubbling - clicked element first then bubbles up -- default
+ * submit event listener
+ * prevent default
+ * how to get a value
  */
-
-const container = document.querySelector(".container");
-const btn = document.querySelector(".btn");
-const heading = document.querySelector(".heading");
-
-function sayHello() {
-  console.log("Hello there.");
-}
-
-const click = "click";
-btn.addEventListener(click, function () {
-  const element = document.createElement("h1");
-  element.classList.add("heading");
-  element.textContent = "This is coming from heading";
-  container.appendChild(element);
-});
-
-heading.addEventListener(click, sayHello);
