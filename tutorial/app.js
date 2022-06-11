624905dcc274f12b2d16608935ecc53adcbfcc39
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-06-11 11:08:20 +0200

    Feature: Extended the example for local storage with multiple valuzes.

--- tutorial/app.js
+++ tutorial/app.js
@@ -17,3 +17,14 @@ localStorage.setItem("friends", JSON.stringify(friends));
  */
 const values = JSON.parse(localStorage.getItem("friends"));
 console.log(values[1]);
+
+let fruits;
+if (localStorage.getItem("fruits")) {
+  fruits = JSON.parse(localStorage.getItem("fruits"));
+} else {
+  fruits = [];
+}
+
+console.log(fruits);
+fruits.push("orange");
+localStorage.setItem("fruits", JSON.stringify(fruits));
