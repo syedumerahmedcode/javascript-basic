d3dd36c21ec58dc50dfa4cbfb4339436fa9700fa
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-06-11 15:08:35 +0200

    Feature: Created an example of setTimeout.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,30 +1,37 @@
 /**
- * JSON.stringify(), JSON.parse()
+ * setTimeout - runs function once after a specific time
+ *
+ * - pass function reference
+ * - duration in ms (1000 ms = 1 sec)
+ * - default 0, will cover more extensively in async/await section
+ * - returns unique identifier
+ * - clearTimeout to cancel
+ * - on window object
  */
-
-const friends = ["john", "peter", "oliver"];
-
-/**
- * We have to do it like this, otherwise the whole
- * array is converetd and treated as a string implicitly.
- */
-localStorage.setItem("friends", JSON.stringify(friends));
-
-/**
- * We need JSON.parse() method. Otherwise, the whole array
- * is treated as a string and we get the character present
- * at the index of the string, not the element of the array.
- */
-const values = JSON.parse(localStorage.getItem("friends"));
-console.log(values[1]);
-
-let fruits;
-if (localStorage.getItem("fruits")) {
-  fruits = JSON.parse(localStorage.getItem("fruits"));
-} else {
-  fruits = [];
+function sayHello() {
+  console.log("Hello John");
 }
 
-console.log(fruits);
-fruits.push("orange");
-localStorage.setItem("fruits", JSON.stringify(fruits));
+setTimeout(sayHello, 5000);
+
+// Will not work since this is a function call, not a function reference
+// hence, the method is called instantaneously.
+setTimeout(sayHello(), 5000);
+
+// alternative approach, ES6Script
+// setTimeout(function () {
+//   console.log("Hello John");
+// }, 5000);
+
+// pass arguments
+function showScore(name, score) {
+  console.log(`Hello ${name}. You score is ${score}.`);
+}
+
+const firstId = setTimeout(showScore, 2000, "John", 65);
+const secondId = setTimeout(showScore, 2000, "Peter", 87);
+
+console.log(firstId);
+console.log(secondId);
+
+clearTimeout(firstId);
