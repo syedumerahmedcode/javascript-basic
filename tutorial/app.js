9946f0279f59c63b0ff29e6ff9b8cd49d4328291
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-06-11 16:32:04 +0200

    Feature: Created the basic conditions for the example of load event.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,16 +1,16 @@
 /**
- * The DOMContentLoaded event fires when the initial HTML document
- * has been completely loaded and parsed, without waiting for
- * stylesheets, images and sub-frames to finish loading.
+ * The load event is fired when the whole page has been loaded
+ * including all dependent resourcessuch as stylesheets and images. This is
+ * in contrast to DOMContentLoaded, which is fired as soon as the page DOM
+ * has been loaded, without waiting for resources to finish loading.
  *
- * window.addEventListener("DOMContentLoaded", function () {
+ * window.addEventListener("load", function () {
  *   // Your code goes here.
  * });
  *
- * document.addEventListener...
  */
 
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
   const heading = document.querySelector("h1");
   console.log(heading);
   heading.style.color = "red";
