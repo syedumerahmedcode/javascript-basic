0378b4032f2647d9ed05df80bb21931800fab0cf
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-06-11 11:03:53 +0200

    Feature: Created an example of JSON stringify and parse methods.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,3 +1,19 @@
 /**
  * JSON.stringify(), JSON.parse()
  */
+
+const friends = ["john", "peter", "oliver"];
+
+/**
+ * We have to do it like this, otherwise the whole
+ * array is converetd and treated as a string implicitly.
+ */
+localStorage.setItem("friends", JSON.stringify(friends));
+
+/**
+ * We need JSON.parse() method. Otherwise, the whole array
+ * is treated as a string and we get the character present
+ * at the index of the string, not the element of the array.
+ */
+const values = JSON.parse(localStorage.getItem("friends"));
+console.log(values[1]);
