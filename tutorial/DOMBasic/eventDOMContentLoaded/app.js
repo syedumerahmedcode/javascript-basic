9db480a9d0b7ee2ba5458fbb502eb10a2ae37d67
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-06-11 16:27:59 +0200

    Feature: Created a backup of eventDOMContentLoaded.

---
+++ tutorial/DOMBasic/eventDOMContentLoaded/app.js
@@ -0,0 +1,17 @@
+/**
+ * The DOMContentLoaded event fires when the initial HTML document
+ * has been completely loaded and parsed, without waiting for
+ * stylesheets, images and sub-frames to finish loading.
+ *
+ * window.addEventListener("DOMContentLoaded", function () {
+ *   // Your code goes here.
+ * });
+ *
+ * document.addEventListener...
+ */
+
+window.addEventListener("DOMContentLoaded", function () {
+  const heading = document.querySelector("h1");
+  console.log(heading);
+  heading.style.color = "red";
+});
