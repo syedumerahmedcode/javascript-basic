738cf5bea84aea8d36e1847e4cc8ade4b733665c
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-06-12 09:47:08 +0200

    Feature: Basic setup for the example showing the use of scroll event.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,17 +1,18 @@
 /**
- * The load event is fired when the whole page has been loaded
- * including all dependent resourcessuch as stylesheets and images. This is
- * in contrast to DOMContentLoaded, which is fired as soon as the page DOM
- * has been loaded, without waiting for resources to finish loading.
- *
- * window.addEventListener("load", function () {
- *   // Your code goes here.
- * });
- *
- */
+ * Scroll Event - on an element or a document
 
-window.addEventListener("load", function () {
-  const img = document.querySelector("img");
-  console.log(img);
-  console.log(img.width);
+
+window.addEventListener("scroll", function () {
+  console.log(window.screenY + "px");
+  console.log(window.screenX + "px");
 });
+
+document
+
+scrollY - returns the number of pixels the document
+is currently scrolled along the vertical axis (pageYOffset)
+
+scrollX - returns the number of pixels the document
+is currently scrolled along the horizontal axis (pageXOffset)
+
+ */
