1ffb7d07cfb3b6a3ef7429b982ceba1f08467963
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-06-12 10:46:45 +0200

    Feature: Created an example for the resize event.

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,23 +1,7 @@
 /**
- * innerWidth - The read-only Windows property
- * The interior width of the Windows in pixels
- *
- * innerHeight - The read-only property of the Windows interface.
- * Returns the interior height of the Windows in pixels.
- *
- * The Element.getBoundingClientRect() method returns a DOMRect opbject
- * providing information about the size of an element and its position
- * relative to the viewport.
- *
+ * The resize event fires when the document view (window) has been resized.
  */
 
-console.log("Height: " + window.innerHeight);
-console.log("Width: " + window.innerWidth);
-
-const btn = document.querySelector(".btn");
-const box = document.querySelector(".box");
-
-btn.addEventListener("click", function () {
-  const values = box.getBoundingClientRect();
-  console.log(values);
+window.addEventListener("resize", function () {
+  console.log(window.innerWidth);
 });
